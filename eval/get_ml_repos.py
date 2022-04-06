553d2403525fcab858f6bfb1e9eb420ba4f674b4
Sebastian Simon <ssimon@informatik.uni-leipzig.de>
2022-04-06 15:09:45 +0200

    Revise repo structure

--- eval/get_ml_repos.py
+++ eval/get_ml_repos.py
@@ -8,6 +8,15 @@ import pandas as pd
 
 target_dir = "\\\\nfs.ceph.dw.webis.de\\cephfs\\data-tmp\\2021\\liju1602\\paperswithcode_crawl"
 
+SKLEARN_REGEX = re.compile(r"import sklearn")
+SKLEARN_FROM_REGEX = re.compile(r"from sklearn[a-zA-z._]* import [a-zA-Z_]*")
+
+TENSORFLOW_REGEX = re.compile(r"import tensorflow")
+TENSORFLOW_FROM_REGEX = re.compile(r"from tensorflow[a-zA-z._]* import [a-zA-Z_]*")
+
+PYTORCH_REGEX = re.compile(r"import torch")
+PYTORCH_FROM_REGEX = re.compile(r"from torch[a-zA-z._]* import [a-zA-Z_]*")
+
 
 def create_sample_set(number):
     for _, _, files in os.walk(target_dir):
@@ -23,17 +32,13 @@ def read_samples(file_path):
         lines = [line.replace("\n", "") for line in lines]
     return lines
 
-def find_scikit_learn_repos():
-    import_regex = re.compile(r"import sklearn")
-    import_from_regex = re.compile(
-            r"from sklearn[a-zA-z._]* import [a-zA-Z_]*"
-        )
+def find_ml_repos(import_regex, import_from_regex, file_path):
     samples = read_samples("./data/sklearn/sample_set.txt")
 
-    with open("./data/sklearn/sklearn_sample.txt", "w") as source:
+    with open(filepath, "w") as source:
         for subdir, _ , files in os.walk(target_dir):
             for file in files:
-                is_scikit_learn_repo = False
+                is_ml_repo = False
                 if file in samples:
                     print("Processing: " + file)
                     tar = tarfile.open(os.path.join(subdir, file))
@@ -46,15 +51,15 @@ def find_scikit_learn_repos():
                                 for line in f.readlines():
                                     if import_regex.search(str(line)):
                                         source.write(file + "\n")
-                                        is_scikit_learn_repo = True
+                                        is_ml_repo = True
                                         break
                                     if import_from_regex.search(str(line)):
                                         source.write(file + "\n")
-                                        is_scikit_learn_repo = True
+                                        is_ml_repo = True
                                         break
                             except (AttributeError, KeyError):
                                 print("skipped: ", filepath)
-                        if is_scikit_learn_repo:
+                        if is_ml_repo:
                             break
 
 def get_urls():
@@ -79,12 +84,13 @@ def get_urls():
 
 if __name__ == "__main__":
     # create_sample_set(1000), already done
-    # find_scikit_learn_repos(), already done
-    # get_urls(), already done
 
-    print(dir)
+    sklearn_path = "data/sklearn/sklearn_sample.txt"
+    tensorflow_path = "data/sklearn/tensorflow_samples.txt"
+    pytorch_path = "data/sklearn/pytorch_samples.txt"
 
-    if os.path.exists(dir):
-        print("true")
-    else:
-        print("false")
+    find_ml_repos(SKLEARN_REGEX, SKLEARN_FROM_REGEX, sklearn_path)
+    find_ml_repos(TENSORFLOW_REGEX, TENSORFLOW_FROM_REGEX, tensorflow_path)
+    find_ml_repos(PYTORCH_REGEX, PYTORCH_FROM_REGEX, pytorch_path)
+
+    # get_urls
