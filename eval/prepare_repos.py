0ba1c39db9210ebfd37e3c2149345a8c0d48e822
Sebastian Simon <ssimon@aspra13.informatik.uni-leipzig.de>
2022-06-23 16:47:30 +0200

    Get all repos

--- eval/prepare_repos.py
+++ eval/prepare_repos.py
@@ -1,14 +1,25 @@
+sklearn_file = "../data/sklearn/sklearn_sample_url_final.csv"
+tf_file = "../data/tensorflow/tensorflow_samples_url_final.csv"
+torch_file = "../data/pytorch/pytorch_samples_url_final.csv"
+
+files = [sklearn_file, tf_file, torch_file]
+
+
 def main():
-    urls = []
+    urls = set()
 
-    with open("../data/sklearn/sklearn_sample_url_final.csv", "r", encoding="utf-8") as src:
-        for line in src.readlines():
-            
-            url = line.split(",")[-1].strip()
-            urls.append(url)
+    for file_name in files:
+        with open(file_name, "r", encoding="utf-8") as src:
+            for line in src.readlines():
+                if "tar_filename" in line or "repo_url" in line:
+                    continue
+                else:
+                    url = line.split(",")[-1].strip()
+                    urls.add(url)
 
 
-        urls = urls[1:]    
+         
     print(len(urls))
+    print(urls)
 if __name__ == "__main__":
     main()
