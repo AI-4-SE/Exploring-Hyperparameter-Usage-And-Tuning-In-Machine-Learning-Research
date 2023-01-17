6c3a2b903b3778b20a304d04ca17c0f22b9555cd
Sebastian Simon <Bastisimon95@googlemail.com>
2023-01-17 18:12:04 +0100

    Compute number of paper from dblp found with keywords

---
+++ src/dblp_results/dblp.py
@@ -0,0 +1,67 @@
+import json
+from typing import List
+import matplotlib.pyplot as plt
+
+hyperparameter_importance = "../data/dblp/hyperparameter_importance.json"
+hyperparameter_tuning = "../data/dblp/hyperparameter_tuning.json"
+hyperparameter_optimization = "../data/dblp/hyperparameter_optimization.json"
+
+files = [hyperparameter_importance, hyperparameter_tuning, hyperparameter_optimization]
+
+def get_hits_by_year(year: str) -> int:
+    matches = set()
+
+    for file_path in files:
+        with open(file_path, "r", encoding="utf-8") as src:
+            data = json.load(src)
+            results = data["result"]
+            hits = results["hits"]
+
+            for hit in hits["hit"]:
+                info = hit["info"]
+
+                if year == info["year"]:
+                    matches.add(info["title"])
+
+                
+
+    return len(matches)
+
+def create_histogram():
+    
+    data = [
+        get_hits_by_year("2015"), 
+        get_hits_by_year("2016"), 
+        get_hits_by_year("2017"),
+        get_hits_by_year("2018"),
+        get_hits_by_year("2019"),
+        get_hits_by_year("2020"),
+        get_hits_by_year("2021"),
+        get_hits_by_year("2022"),
+    ]
+
+    labels = ["2015", "2016", "2017", "2018", "2019", "2020", "2021", "2022"]
+    
+    fig, ax = plt.subplots()
+
+    ax.bar(labels, data)
+
+    ax.set_xlabel('Year')
+    ax.set_ylabel('Number of Papers')
+
+    #plt.tight_layout(pad=0)
+    plt.savefig("paper_per_year.svg",bbox_inches='tight',  pad_inches=0)
+
+def main():
+    print("Matches 2015: ", get_hits_by_year("2015"))
+    print("Matches 2016: ", get_hits_by_year("2016"))
+    print("Matches 2017: ", get_hits_by_year("2017"))
+    print("Matches 2018: ", get_hits_by_year("2018"))
+    print("Matches 2019: ", get_hits_by_year("2019"))
+    print("Matches 2020: ", get_hits_by_year("2020"))
+    print("Matches 2021: ", get_hits_by_year("2021"))
+    print("Matches 2022: ", get_hits_by_year("2022"))
+
+
+if __name__ == "__main__":
+    create_histogram()
