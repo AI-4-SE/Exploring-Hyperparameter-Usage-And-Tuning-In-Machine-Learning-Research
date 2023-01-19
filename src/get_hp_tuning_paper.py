2fb1f6aee001e75bbe76577e055c86b0d69b957e
Sebastian Simon <Bastisimon95@googlemail.com>
2023-01-19 15:52:19 +0100

    Move scripts from results folder in src folder

--- src/get_hp_tuning_paper.py
+++ src/get_hp_tuning_paper.py
@@ -3,7 +3,7 @@ import glob
 import json
 from typing import List
 
-file_path = "../data/paper_analysis/metadata-with-categories-cleaned.csv"
+file_path = "../../data/paper_analysis/metadata-with-categories-cleaned.csv"
 
 def get_hp_papers() -> pd.DataFrame:
 
@@ -18,7 +18,7 @@ def get_statistics_files(df: pd.DataFrame) -> List[str]:
 
     final_statistics_files = []
 
-    statistic_files = glob.glob("../data/statistics/**")
+    statistic_files = glob.glob("../../data/statistics/**")
     #print(len(statistic_files))
 
     urls = df["repo_url"].tolist()
@@ -33,7 +33,7 @@ def get_statistics_files(df: pd.DataFrame) -> List[str]:
     #print(final_statistics_files)
     #print(len(final_statistics_files))
 
-    with open("../data/repos_hyperparameter_tuning.json", "w", encoding="utf-8") as dest:
+    with open("../../data/repos_hyperparameter_tuning.json", "w", encoding="utf-8") as dest:
         json.dump(final_statistics_files, dest, sort_keys=True, indent=4)
 
 
